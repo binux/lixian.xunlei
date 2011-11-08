8012345742e500f478667c0363543feac81df318
Binux <17175297.hk@gmail.com>
2011-11-09 00:37:33 +0800

    jsparser will return a python object now, add more apis:
    get_task_list
    add_bt_task
    add_bt_task_with_dict
    add_task
    get_bt_list
    delete_task
    delay_task
    find_task_by_cid

--- jsfunctionParser.py
+++ jsfunctionParser.py
@@ -31,6 +31,7 @@ value
     null
 """
 
+import json
 from pyparsing import *
 
 TRUE = Keyword("true").setParseAction( replaceWith(True) )
@@ -49,15 +50,12 @@ jsonElements = delimitedList( jsonValue )
 jsonArray1 = Group(Suppress('[') + Optional(jsonElements) + Suppress(']') )
 jsonArray2 = Group(Suppress(CaselessLiteral('new'))+Suppress(White())+Suppress(CaselessLiteral('array(')) + Optional(jsonElements) + Suppress(')') )
 jsonArray << ( jsonArray1 | jsonArray2 )
-jsonValue << ( jsonString | jsonNumber | Group(jsonObject)  | jsonArray | TRUE | FALSE | NULL )
+jsonValue << ( jsonString | jsonNumber | jsonObject | jsonArray | TRUE | FALSE | NULL )
 memberDef = Group( jsonString + Suppress(':') + jsonValue )
 jsonMembers = delimitedList( memberDef )
 jsonObject << Dict( Suppress('{') + Optional(jsonMembers) + Suppress('}') )
 jsFunctionName = Word(alphas + "_.",  alphanums + "_.")
 jsFunctionCall = jsFunctionName+Suppress("(")+Group(Optional(jsonElements))+Suppress(")"+Optional(";"))
-
-jsonComment = cppStyleComment 
-jsonObject.ignore( jsonComment )
 
 def convertNumbers(s,l,toks):
     n = toks[0]
@@ -65,8 +63,22 @@ def convertNumbers(s,l,toks):
         return int(n)
     except ValueError, ve:
         return float(n)
-        
+
+def convertDict(toks):
+    result = {}
+    for each in toks:
+        result[each[0]] = each[1]
+    return result
+
+def convertList(toks):
+    result = []
+    for each in toks:
+        result.append(each.asList())
+    return result
+
 jsonNumber.setParseAction( convertNumbers )
+jsonObject.setParseAction( convertDict )
+jsonArray.setParseAction( convertList )
 
 def parser_js_function_call(string):
     return jsFunctionCall.parseString(string).asList()
@@ -75,7 +87,9 @@ if __name__ == "__main__":
     testdata = """queryUrl(1,'3BC3BEC436094736BF19350711D4A5556F4B7536','123806924','[Dymy][Nurarihyon_no_Mago_Sennen_Makyou][18v2][BIG5][RV10][848x480].rmvb','0',new Array('[Dymy][Nurarihyon_no_Mago_Sennen_Makyou][18v2][BIG5][RV10][848x480].rmvb'),new Array('118M'),new Array('123806924'),new Array('1'),new Array('RMVB'),new Array('0'),'13206731991951250210.5123566347')"""
     testdata1 = """ queryCid('4E1FA9C76605CA8E77DD35DA08D817617403BF26', '87B9431F2F5606721BD761FAA5638A809DAF3080', '8055706', 'hbzz.rar', 0, 0, 0,'13206731991951250210.5123566347','rar')"""
     testdata2 = """ parent.begin_task_batch_resp()"""
+    testdata3 = """fill_bt_list({"Result":{"Tid":"33684635655","Infoid":"3547930B96AFA7B0A1CFCC80D516ADE97A34DAE0","Record":[{"id":0,"title":"[AngelSub][Guilty Crown][04][FULLHD][BIG5][x264_AC3].mkv","download_status":"1","cid":"DCA5280E5072F48A2D0059F9BD00077395542202","size":"610M","percent":85.8,"taskid":"33684635975","icon":"RMVB","livetime":"7\u5929","downurl":"","vod":"0","cdn":[],"format_img":"video","filesize":"640205218","verify":"","url":"bt:\/\/3547930B96AFA7B0A1CFCC80D516ADE97A34DAE0\/0","openformat":"movie","ext":"mkv","dirtitle":"[AngelSub][Guilty Crown][04][FULLHD][BIG5][x264_AC3].mkv"}]}})"""
     import pprint
     pprint.pprint( parser_js_function_call(testdata) )
     pprint.pprint( parser_js_function_call(testdata1) )
     pprint.pprint( parser_js_function_call(testdata2) )
+    pprint.pprint( parser_js_function_call(testdata3) )
--- lixian_api.py
+++ lixian_api.py
@@ -101,6 +101,27 @@ class LiXianAPI(object):
                 break
         return self._task_url
 
+    def get_task_list(self, pagenum=1000):
+        r = self.session.get(self.task_url(), cookies=dict(pagenum=str(pagenum)))
+        if r.error:
+            r.raise_for_status()
+        soup = BeautifulSoup(r.content)
+        result = []
+        for task in soup.findAll("div", **{"class": "rw_list"}):
+            tmp = dict()
+            for each in task.findAll("input"):
+                input_id = each.get("id", "")
+                if not input_id: continue
+                input_attr = input_id.rstrip("1234567890")
+                input_value = each.get("value", "")
+                tmp[input_attr] = input_value
+            if "input" in tmp:
+                tmp["id"] = tmp["input"]
+            else:
+                raise Exception, "can't find task_id %r" % tmp
+            result.append(tmp)
+        return result 
+
     QUERY_URL = "http://dynamic.cloud.vip.xunlei.com/interface/url_query?callback=queryUrl&u=%(url)s&random=%(random)s&tcache=%(cachetime)d"
     def bt_task_check(self, url):
         r = self.session.get(self.QUERY_URL % {"url": urllib.quote_plus(url),
@@ -126,21 +147,35 @@ class LiXianAPI(object):
                 random = args[11])
 
     BT_TASK_COMMIT_URL = "http://dynamic.cloud.vip.xunlei.com/interface/bt_task_commit"
-    def add_bt_task(self):
-        #createNewFormElement(f, "uid", $PU("userid"));
-        #createNewFormElement(f, "btname", btname);
-        #createNewFormElement(f, "cid", gCID);
-        #createNewFormElement(f, "goldbean", gGoldBean);
-        #createNewFormElement(f, "silverbean", gSilverBean);
-        #createNewFormElement(f, "tsize", gBtSize);
-        #createNewFormElement(f, "findex",index_list);
-        #createNewFormElement(f, "size",size_list);
-        #createNewFormElement(f, "name",gname.value); // title_list
-        #createNewFormElement(f, "o_taskid",delete_id);
-        #createNewFormElement(f, "o_page",G_PAGE);
-        #createNewFormElement(f, "from","0");        
-        # TODO
-        pass
+    def add_bt_task(self, url, add_all=True):
+        info = self.bt_task_check(url)
+        if not info: return False
+        if add_all:
+            for i, v in enumerate(info["valid_list"]):
+                if v == "0":
+                    info["valid_list"][i] = "1"
+        return self.add_bt_task_with_dict(info)
+
+    def add_bt_task_with_dict(self, info):
+        data = dict(
+                uid = self.uid,
+                btname = info["bt_title"],
+                cid = info["infohash"],
+                goldbean = 0,
+                silverbean = 0,
+                tsize = info["fsize"],
+                findex = "_".join([x for i, x in enumerate(info["findex"]) if info["valid_list"][i] == "1"]),
+                size = "_".join([x for i, x in enumerate(info["size_list"]) if info["valid_list"][i] == "1"]),
+                name = "undefined",
+                o_taskid = 0,
+                o_page = "task")
+        data["from"] = 0
+        r = self.session.post(self.BT_TASK_COMMIT_URL, data)
+        if r.error:
+            r.raise_for_status()
+        if "top.location" in r.content:
+            return True
+        return False
 
     TASK_CHECK_URL = "http://dynamic.cloud.vip.xunlei.com/interface/task_check?callback=queryCid&url=%(url)s&random=%(random)s&tcache=%(cachetime)d"
     def task_check(self, url):
@@ -163,10 +198,19 @@ class LiXianAPI(object):
             is_full = args[6],
             random = args[7])
 
-    TASK_COMMIT_URL = "http://dynamic.cloud.vip.xunlei.com/interface/task_commit?callback=ret_task&uid=%(uid)s&cid=%(cid)s&gcid=%(gcid)s&size=%(file_size)s&goldbean=%(goldbean_need)s&silverbean=%(silverbean_need)s&t=%(tname)s&url=%(url)s&type=%(type)s&o_page=task&o_taskid=0"
+    TASK_COMMIT_URL = "http://dynamic.cloud.vip.xunlei.com/interface/task_commit?callback=ret_task&uid=%(uid)s&cid=%(cid)s&gcid=%(gcid)s&size=%(file_size)s&goldbean=%(goldbean_need)s&silverbean=%(silverbean_need)s&t=%(tname)s&url=%(url)s&type=%(task_type)s&o_page=task&o_taskid=0"
     def add_task(self, url):
-        #TODO
-        pass
+        info = self.task_check(url)
+        if not info: return False
+        info["uid"] = self.uid
+        info["url"] = urllib.quote_plus(url)
+        info["task_type"] = 0
+        r = self.session.get(self.TASK_COMMIT_URL % info)
+        if r.error:
+            r.raise_for_status()
+        if "top.location" in r.content:
+            return True
+        return False
 
     BATCH_TASK_CHECK_URL = "http://dynamic.cloud.vip.xunlei.com/interface/batch_task_check"
     def batch_task_check(self, url_list):
@@ -194,6 +238,83 @@ class LiXianAPI(object):
             r.raise_for_status()
         if "top.location" in r.content:
             return True
+
+    FILL_BT_LIST = "http://dynamic.cloud.vip.xunlei.com/interface/fill_bt_list?callback=fill_bt_list&tid=%(tid)s&infoid=%(cid)s&g_net=1&p=1&uid=%(uid)s&noCacheIE=%(cachetime)d"
+    def get_bt_list(self, tid, cid):
+        r = self.session.get(self.FILL_BT_LIST % dict(
+                                tid = tid,
+                                cid = cid,
+                                uid = self.uid,
+                                cachetime = self._now))
+        if r.error:
+            r.raise_for_status()
+        function, args = parser_js_function_call(r.content)
+        if not args:
+            return {}
+        if isinstance(args[0], str):
+            raise Exception, args[0]
+        return args[0].get("Result", {})
+
+    TASK_DELAY_URL = "http://dynamic.cloud.vip.xunlei.com/interface/task_delay?taskids=%(ids)s&noCacheIE=%(cachetime)d"
+    def delay_task(self, task_ids):
+        tmp_ids = [str(x)+"_1" for x in task_ids]
+        r = self.session.get(self.TASK_DELAY_URL % dict(
+                            ids = ",".join(tmp_ids),
+                            cachetime = self._now))
+        if r.error:
+            r.raise_for_status()
+        function, args = parser_js_function_call(r.content)
+        if not args: return False
+        if args[0].get("result") == 1:
+            return True
+        return False
+
+    TASK_DELETE_URL = "http://dynamic.cloud.vip.xunlei.com/interface/task_delete?type=0&taskids=%(ids)s&noCacheIE=%(cachetime)d"
+    def delete_task(self, task_ids):
+        r = self.session.get(self.TASK_DELETE_URL % dict(
+                            ids = urllib.quote_plus(",".join(task_ids)),
+                            cachetime = self._now))
+        if r.error:
+            r.raise_for_status()
+        function, args = parser_js_function_call(r.content)
+        if not args: return False
+        if args[0].get("result") == 1:
+            return True
+        return False
+
+    def find_task_by_cid(self, cid, task_list=None):
+        if task_list is None:
+            task_list = self.get_task_list()
+        for task in task_list:
+            if task.get("dcid", "") == cid:
+                return task
+
+    SHARE_URL = "http://dynamic.sendfile.vip.xunlei.com/interface/lixian_forwarding"
+    def share(self, email, tasks, msg="", task_list=None):
+        cookies = requests.utils.dict_from_cookiejar(self.session.cookies)
+        sessionid = cookies.get("sessionid", None)
+        if task_list is None:
+            task_list = self.get_task_list()
+        payload = []
+        i = 0
+        for task in task_list:
+            if task["id"] in tasks:
+                tmp = {
+                    "cid_%d" % i : task["dcid"],
+                    "file_size_%d" % i : task["ysfilesize"],
+                    "gcid_%d" % i : "", #TODO
+                    "url_%d" % i : task["f_url"],
+                    "title_%d" % i : task["durl"],
+                    "section_%d" % i : ""} #TODO
+                i += 1
+                payload.append(tmp)
+
+        data = dict(
+                uid = self.uid,
+                sessionid = sessionid,
+                msg = msg,
+                resv_email = ";".join(email),
+                data = json.dumps(payload))
 
     def check_login(self):
         pass
