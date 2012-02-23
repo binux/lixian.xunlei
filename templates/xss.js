ca3b3364560c2bdc6f5a1bf2dc8fe824a94f117c
Binux <17175297.hk@gmail.com>
2012-02-23 23:23:27 +0800

    remove sample url in xss.js

--- templates/xss.js
+++ templates/xss.js
@@ -9,7 +9,6 @@ function xss() {
     $.fancybox('<div style="width:300px;"><p style="color: red;">Cookie写入失败...</p><p>您可能无法使用浏览器下载功能</p><p><a href="javascript:location.reload();">刷新重试</a>，或向作者回报这个问题：<a href="http://gplus.to/binux">+足兆叉虫</a></p></div>', {padding: 20, onClosed: function () { document.cookie = "xss=done;"; }});
     return;
   }
-  // http://dynamic.cloud.vip.xunlei.com/interface/task_process?callback=%3Cscript%20src=task_process%26callback=function%2520a%28%29%257Beval%28String.fromCharCode%2897%2C108%2C101%2C114%2C116%2C40%2C34%2C115%2C101%2C116%2C32%2C99%2C111%2C111%2C107%2C105%2C101%2C33%2C34%2C41%29%29%3B%257D%2520a%3E%3C/script%3E
   var script = 'document.cookie="{{ cookie }}";document.write("<iframe src=\\\"{{ request.protocol }}://{{ request.host }}/xss\\\" />");';
   var code = [];
   for (var i=0;i<script.length;i++) {
