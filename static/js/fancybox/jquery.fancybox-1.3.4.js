6304dcbfd409f25e01a9fe429fac6e0a052829c7
Binux <17175297.hk@gmail.com>
2012-05-10 00:39:24 +0800

    fix xss of fancybox

--- static/js/fancybox/jquery.fancybox-1.3.4.js
+++ static/js/fancybox/jquery.fancybox-1.3.4.js
@@ -464,10 +464,10 @@
 		_format_title = function(title) {
 			if (title && title.length) {
 				if (currentOpts.titlePosition == 'float') {
-					return '<table id="fancybox-title-float-wrap" cellpadding="0" cellspacing="0"><tr><td id="fancybox-title-float-left"></td><td id="fancybox-title-float-main">' + title + '</td><td id="fancybox-title-float-right"></td></tr></table>';
+					return '<table id="fancybox-title-float-wrap" cellpadding="0" cellspacing="0"><tr><td id="fancybox-title-float-left"></td><td id="fancybox-title-float-main">' + $("<i></i>").text(title).html() + '</td><td id="fancybox-title-float-right"></td></tr></table>';
 				}
 
-				return '<div id="fancybox-title-' + currentOpts.titlePosition + '">' + title + '</div>';
+				return '<div id="fancybox-title-' + currentOpts.titlePosition + '">' + $("<i></i>").text(title).html() + '</div>';
 			}
 
 			return false;
