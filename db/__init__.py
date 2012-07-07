c85426af97f50d753c1a053f2fa875f2d56528e9
Binux <17175297.hk@gmail.com>
2012-07-07 10:39:07 +0800

    Revert "a bigger sql connection pool"
    This reverts commit fc0aa00f1f27c500f10cc16b10021aa10877d5e8.
    fix #sqlit

--- db/__init__.py
+++ db/__init__.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.exc import SQLAlchemyError
 
-engine = create_engine(options.database_engine, echo=options.database_echo, max_overflow=25, pool_timeout=1, pool_recycle=3600)
+engine = create_engine(options.database_engine, echo=options.database_echo, pool_recycle=3600)
 
 metadata = Base.metadata
 metadata.create_all(engine)
